96d606726a838948275c7d0693c3f4756ae710f2
ari <ari.orre@aalto.fi>
2021-06-23 23:04:31 +0300

    back

--- drone_infer.sh
+++ drone_infer.sh
@@ -1,4 +1,4 @@
 python tools/test_net.py \
-    --config-file fcos/configs/fcos_R_101_FPN_2x_person8.yaml\                  
+    --config-file configs/fcos/fcos_R_101_FPN_2x_person8.yaml \
     MODEL.WEIGHT trained_models/fcos_R_101_FPN_2x_person8/model_final.pth \
     TEST.IMS_PER_BATCH 4
